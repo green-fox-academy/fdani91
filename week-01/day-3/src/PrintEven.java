224ee50aa5ad716bb9aa950799fb4ea4353b8458
Daniel Fodor <daniel.fodor91@gmail.com>
2019-04-04 17:56:14 +0200

    added PrintEven

---
+++ week-01/day-3/src/PrintEven.java
@@ -0,0 +1,14 @@
+public class PrintEven {
+    public static void main(String[] args) {
+
+        // Create a program that prints all the even numbers between 0 and 500
+
+        int b = 0;
+
+        while (b <= 500) {
+            System.out.println(b);
+            b = b + 2;
+        }
+
+    }
+}
