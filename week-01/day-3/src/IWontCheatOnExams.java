15ab3447dfae7eb204213faca1fca33284768bf3
Daniel Fodor <daniel.fodor91@gmail.com>
2019-04-04 17:34:19 +0200

    added IWontCheatOnExams

---
+++ week-01/day-3/src/IWontCheatOnExams.java
@@ -0,0 +1,15 @@
+public class IWontCheatOnExams {
+    public static void main(String[] args) {
+
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+
+        int a = 0;
+
+        while (a < 100) {
+            System.out.println("I won't cheat on the exam!");
+            a++;
+        }
+
+    }
+}
