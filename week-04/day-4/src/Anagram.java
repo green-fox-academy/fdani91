71cb993b9bdc0613574bce6277a2a30ae9cd8858
Daniel Fodor <daniel.fodor91@gmail.com>
2019-04-25 17:07:44 +0200

    updated task info in Anagra:m

--- week-04/day-4/src/Anagram.java
+++ week-04/day-4/src/Anagram.java
@@ -1,3 +1,7 @@
+// Write a function, that takes two strings and returns a boolean value based on if the two strings
+// are Anagramms or not.
+// Create a test for that.
+
 import java.util.Arrays;
 
 public class Anagram {
